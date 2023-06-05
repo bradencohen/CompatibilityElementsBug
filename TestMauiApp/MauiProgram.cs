da6f19814aebd1f8920ea4ffa926d2225a2675b2
braden <braden.cohen@Yahoo.com>
2023-06-05 09:35:04 -0700

    Add project files.

---
+++ TestMauiApp/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+namespace TestMauiApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+            .UseMauiCompatibility()
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.Services.AddSingleton<MainPage>();
+
+		return builder.Build();
+	}
+}
---
+++ TestMauiApp/Views/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestMauiApp.Views;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
